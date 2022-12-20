4f58213f032b4fc29ee2ca1f138c0acc7deec235
PB <52748502+verdaux@users.noreply.github.com>
2022-12-20 18:48:10 +0530

    Sample program to iterate a list created.

---
+++ FunctionalProgrammingPrac/src/main/java/com/sample/Structured.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+import java.util.List;
+
+public class Structured
+{
+	public static void main(String[] args)
+	{
+		printAllNumbersInListStructured(List.of(12,9,13,4,6,2,4,12,15));
+	}
+
+	private static void printAllNumbersInListStructured(List<Integer> numbers)
+	{
+		for(int number:numbers)
+		{
+			System.out.println(number);
+		}
+	}
+}
